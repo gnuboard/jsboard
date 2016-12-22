eebfe701ca9568415143542a2117f8790da951ed
AHNJUNGMO\S7 <lalala13@nate.com>
2016-12-22 11:27:14 +0900

    testcontroller

---
+++ src/main/java/kr/sir/controller/TestController.java
@@ -0,0 +1,13 @@
+package kr.sir.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@RequestMapping("/")
+	public String test() {
+		return "hello jsboard!";
+	}
+}
