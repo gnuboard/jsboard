83359e54adfde451f29ddcc7f33c0946e8d61aa1
AHNJUNGMO\S7 <lalala13@nate.com>
2016-12-22 11:14:21 +0900

    jsboard

---
+++ src/main/java/kr/sir/JsboardApplication.java
@@ -0,0 +1,12 @@
+package kr.sir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsboardApplication.class, args);
+	}
+}
